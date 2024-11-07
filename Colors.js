fda9dab765d8c666ef7f6e87d9a744f2e3d288d6
Clarence21-ai <agtualclarence16@gmail.com>
2024-11-07 16:02:31 +0000

    Initial commit with app.js

---
+++ Colors.js
@@ -0,0 +1,11 @@
+let colors = ["red", "blue", "green"];
+
+console.log(colors);
+
+function addColor() {
+    let newColor = prompt("Enter a color you like:");
+        colors.push(newColor);
+            console.log(colors); 
+            }
+
+            addColor(); 
